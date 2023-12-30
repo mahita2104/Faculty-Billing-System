e3e6efc06c58540415819ae2a17fb6061489e0f2
Anamika Kumari <122178637+Anamika-K20@users.noreply.github.com>
2023-12-30 13:36:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
-
+// src/App.tsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import FacultyDetails from "./components/FacultyDetails";
+import EvaluationPage from "./pages/EvaluationPage";
+import QuestionPaper from "./pages/QuestionPaper";
+import Signup from "./pages/signup";
 import Login from "./pages/login";
-import SignUp from "./pages/signup";
-import Header from './components/header';
+import Header from "./components/header";
+
+import "./App.css";
 
 const App: React.FC = () => {
+  const facultyDetails = {
+    name: "John Doe",
+    address: "123 Main St, City",
+    designation: "Professor",
+    email: "john.doe@example.com",
+    mobileNo: "9876543210",
+    alternateNo: "9876543211",
+    bankName: "Sample Bank",
+    branchName: "City Branch",
+    accountNo: "1234567890",
+    ifscCode: "ABCD1234567",
+  };
+
   return (
-    
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        {/* Add more routes as needed */}
-      </Routes>
+      <div className="app">
+        <Sidebar setActiveTab={() => {}} />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<FacultyDetails {...facultyDetails} />} />
+            <Route path="/evaluation" element={<EvaluationPage />} />
+            <Route path="/questionPaper" element={<QuestionPaper />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
