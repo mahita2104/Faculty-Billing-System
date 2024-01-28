f3b988e051c9ba85ed10515b28c2ead1fffed889
Anamika Kumari <122178637+Anamika-K20@users.noreply.github.com>
2024-01-28 20:51:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,12 @@
-// src/App.tsx
-/* import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import FacultyDetails from "./components/FacultyDetails";
-import EvaluationPage from "./pages/EvaluationPage";
-import QuestionPaper from "./pages/QuestionPaper";
-import Signup from "./pages/signup";
-import Login from "./pages/login";
-import Header from "./components/header";
-import "./App.css"; // Import the CSS file
-
-const App: React.FC = () => {
-  const facultyDetails = {
-    name: "John Doe",
-    address: "123 Main St, City",
-    designation: "Professor",
-    email: "john.doe@example.com",
-    mobileNo: "9876543210",
-    alternateNo: "9876543211",
-    bankName: "Sample Bank",
-    branchName: "City Branch",
-    accountNo: "1234567890",
-    ifscCode: "ABCD1234567",
-  };
-
-  return (
-    <Router>
-      <div className="app">
-        <Sidebar setActiveTab={() => {}} />
-        <div className="main-content">
-          <Routes>
-            <Route path="/" element={<FacultyDetails {...facultyDetails} />} />
-            <Route path="/evaluation" element={<EvaluationPage />} />
-            <Route path="/questionPaper" element={<QuestionPaper />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
-};
-
-export default App; */
-// src/App.tsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import FacultyDetails from "./components/FacultyDetails";
 import EvaluationPage from "./pages/EvaluationPage";
 import QuestionPaper from "./pages/QuestionPaper";
+import PracticalPage from "./pages/Practical";
+//import noofhours from "./pages/noofhours";
+//import conveyance from "./pages/conveyance";
 import Signup from "./pages/signup";
 import Login from "./pages/login";
 import Header from "./components/header";
@@ -82,6 +39,8 @@ const App: React.FC = () => {
               element={<FacultyDetails {...facultyDetails} />}
             />
             <Route path="/evaluation" element={<EvaluationPage />} />
+            <Route path="/practical" element={<PracticalPage />} />
+
             <Route path="/questionPaper" element={<QuestionPaper />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
@@ -93,4 +52,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
